c487a38242a993ec099c876f9aebe908055c6361
Joao Victor Rocha <jvpereirarocha@gmail.com>
2021-01-03 23:30:28 -0300

    Lessions about loopings with for, while and do while

---
+++ fundamentals/loopings.js
@@ -0,0 +1,27 @@
+//for
+for (var i = 0; i < 10; i++) {
+  console.log(i); //Output 0, 1, 2, 3... 9
+}
+
+for (var j = 10; j > 0; j--) {
+  console.log(j); //Output 10, 9, 8... 1
+}
+
+
+console.log('\n\n');
+
+//while
+var k = 0;
+while (k < 10) {
+  console.log(k);
+  k++;
+}
+
+
+console.log('\n\n');
+//do while
+var z = 10;
+do {
+  console.log(z);
+  z--;
+} while(z > 0);
