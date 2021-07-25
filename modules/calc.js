0272ff9b3298ee65619de9410434357f6924a45c
Joao Victor Rocha <jvpereirarocha@gmail.com>
2021-07-25 13:05:06 -0300

    lessions about common js module

---
+++ modules/calc.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const circleArea = r => 3.14 * (r ** 2);
+const squareArea = s => s * s;
+
+module.exports = {
+  circleArea, squareArea
+}; //Common JS exports
---
+++ modules/main.js
@@ -0,0 +1,4 @@
+const { circleArea, squareArea } = require('./calc.js'); // CommonJs import
+
+console.log(circleArea(2));
+console.log(squareArea(2));
